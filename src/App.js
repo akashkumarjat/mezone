9fe5adce09cfc17c83ac3aecd7231b2513681245
akash <akashkumarjat@gmail.com>
2022-09-12 21:22:57 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './compenents/Home';
+import Header from './compenents/Header'
+import "./assets/index.css";
+import Footer from './compenents/Footer';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Allproducts from './compenents/Allproducts';
+import Contactus from './compenents/Contactus';
+import Aboutus from './compenents/Aboutus';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={< Home />} />
+          <Route path='/Allproducts' element={< Allproducts />} />
+          <Route path='/contactus' element={< Contactus />} />
+          <Route path='/Aboutus' element={< Aboutus />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+  )
 }
-
-export default App;
+export default App
